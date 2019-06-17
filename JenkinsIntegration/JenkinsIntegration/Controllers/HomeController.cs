b4b0641d17b651c467527197fbe0a50537ae46a8
Akhil Mittal <akhil.mittal20@gmail.com>
2019-06-17 18:37:09 +0530

    Source Code

---
+++ JenkinsIntegration/JenkinsIntegration/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JenkinsIntegration.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly IDataService _dataService;
+
+        public HomeController(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["HostName"] = _dataService.GetHost();
+            return View();
+        }
+    }
+}
---
+++ JenkinsIntegration/JenkinsIntegrationTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services;
+
+namespace JenkinsIntegrationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IDataService dataService = new DataService();
+            var hostName = dataService.GetHost();
+            Assert.AreEqual(Environment.MachineName, hostName);
+        }
+    }
+}
